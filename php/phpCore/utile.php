16ea4e676ff322d3ee870a7c50d08a06e6b6071a
GuillaumeB81 <bguillaume81@gmail.com>
2017-10-30 16:51:54 +0100

    Design de la page personnages aléatoires

--- php/phpCore/utile.php
+++ php/phpCore/utile.php
@@ -22,6 +22,8 @@ function displayHeader($title)
               <title>Random Pick LOL</title>
               <meta name="description" content="">
               <meta name="viewport" content="width=device-width, initial-scale=1">
+              <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+              <link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet">
               <link rel="stylesheet" href="' . WEBROOT . 'css/main.css" id="css">
               <link rel="icon" type="image/png" href="" />
 
@@ -54,10 +56,27 @@ function displayEntete($urlBackground)
             <div id="contenu">';
 }
 
+function displayEnteteAlea($urlBackground)
+{
+    echo '<!doctype html>';
+    displayHeader("Random Pick LOL");
+    echo '<body style="margin:0;background: url(\'' . $urlBackground . '\') no-repeat center fixed; background-size: cover;color: #cac1bd;font-family: Cinzel, Helvetica;overflow-x: hidden;">
+    
+            <div id="contenu">';
+}
+
 function displayFin()
 {
     echo '</div>
         </div>';
+    displayFooter();
+    echo '</body>
+</html>';
+}
+
+function displayFinAlea()
+{
+    echo '</div>';
     displayFooter();
     echo '</body>
 </html>';
--- php/view/aleatoireV.php
+++ php/view/aleatoireV.php
@@ -1,45 +1,116 @@
 <?php
 
-displayEntete("http://ddragon.leagueoflegends.com/cdn/img/champion/splash/" . str_replace('.png', '_0.jpg', $champion->getImage()));
-    echo '<div class="col-md-6">';
-    echo '<img id="' . $champion->getNameId() . '" class="img img-responsive imgChampMedium"
-                src="http://ddragon.leagueoflegends.com/cdn/img/champion/loading/' . str_replace('.png', '_0.jpg', $champion->getImage()) . '" title="' . $champion->getName() . '"
-                alt="' . $champion->getName() . '">';
-    echo '<br/>';
-    echo '</div>';
-    echo '<div class="col-md-6">';
+displayEnteteAlea("http://ddragon.leagueoflegends.com/cdn/img/champion/splash/" . str_replace('.png', '_0.jpg', $champion->getImage()));
 
-    // tableau pour l'assignement des sorts à maxer
-    $arrayChiffre = array(1, 2, 3);
-    shuffle($arrayChiffre);
+echo '<div class="row" >
+        <div class="infos col-xl-4">
+        <div class="row sectionInfos" style="margin-bottom: 15%;margin-top:15%">
+            <div class="col-sm-1">
+            </div>
+            <div class="col-sm-4">
+                <img id="' . $champion->getNameId() . '" class="img imgChampAlea"
+                src="http://ddragon.leagueoflegends.com/cdn/7.20.2/img/champion/' . $champion->getImage() . '" title="' . $champion->getName() . '"
+                alt="' . $champion->getName() . '">
+            </div>
+            <div class="col-sm-7">
+                <h1>'. $champion->getName() .'</h1>
+                <span class="infosLane">Top Lane - Tank</span>
+            </div>
+        </div>';
 
-    $i = 0;
-    foreach($champion->getSpells() as $sort)
-    {
-        echo '<img id="' . $sort->getName() . '" class="sumSpell"
+
+/* ____________________________________ SKILLS A MAX ALEATOIRE _______________________________*/
+// tableau pour l'assignement des sorts à maxer
+$arrayChiffre = array(1, 2, 3);
+shuffle($arrayChiffre);
+
+$i = 0;
+
+echo ' <div class="row sectionInfos">
+            <div class="col-sm-1">
+            </div>
+            <div class="col-sm-10">
+                <h3>Skills</h3>
+                <div class="row">';
+foreach($champion->getSpells() as $sort)
+{
+    echo '<div class="col-sm-3">
+            <img id="' . $sort->getName() . '" class="sumSpell "
                 src="http://ddragon.leagueoflegends.com/cdn/7.20.2/img/spell/' . $sort->getImage() . '" title="' . $sort->getName() . '"
                 alt="' . $sort->getName() . '">';
-        echo $i < 3 ? ' ' . $arrayChiffre[$i] : '';
-        echo '<br/>';
-        ++$i;
-    }
-
-    echo '<br/><br/><img id="' . $sumSpell1->getId() . '" class="sumSpell"
-                src="http://ddragon.leagueoflegends.com/cdn/7.20.2/img/spell/' . $sumSpell1->getImage() . '" title="' . $sumSpell1->getName() . '"
-                alt="' . $sumSpell1->getName() . '"><br/>';
-
-    echo '<img id="' . $sumSpell2->getId() . '" class="sumSpell"
-                src="http://ddragon.leagueoflegends.com/cdn/7.20.2/img/spell/' . $sumSpell2->getImage() . '" title="' . $sumSpell2->getName() . '"
-                alt="' . $sumSpell2->getName() . '">';
+    echo $i < 3 ? ' ' . $arrayChiffre[$i] : '';
     echo '</div>';
+    ++$i;
+}
 
-    echo '<br/><br/>';
+echo '        </div>
+           </div>
+        </div>';
 
-    foreach($items as $item)
-    {
-        echo '<img id="' . $item->getName() . '" class="imgItem"
-                    src="http://ddragon.leagueoflegends.com/cdn/7.20.2/img/item/' . $item->getImage() . '" title="' . $item->getName() . '"
-                    alt="' . $item->getName() . '">';
+
+/* ____________________________________ SUMMONERS SPELL ALEATOIRE _______________________________*/
+
+echo '<div class="row sectionInfos">
+            <div class="col-sm-1">
+            </div>
+            <div class="col-sm-10">
+                <h3>Summoners</h3>
+                <div class="row">';
+
+echo '<div class="col-sm-6">
+                <img id="' . $sumSpell1->getId() . '" class="sumSpell "
+                src="http://ddragon.leagueoflegends.com/cdn/7.20.2/img/spell/' . $sumSpell1->getImage() . '" title="' . $sumSpell1->getName() . '"
+                alt="' . $sumSpell1->getName() . '"></div>';
+
+
+echo '<div class="col-sm-6">
+                <img id="' . $sumSpell2->getId() . '" class="sumSpell "
+                src="http://ddragon.leagueoflegends.com/cdn/7.20.2/img/spell/' . $sumSpell2->getImage() . '" title="' . $sumSpell2->getName() . '"
+                alt="' . $sumSpell2->getName() . '"></div>';
+
+
+echo '          </div>
+            </div>
+        </div>';
+
+
+/* ____________________________________ ITEMS ALEATOIRE _______________________________*/
+
+
+echo '<div class="row sectionInfos">
+            <div class="col-sm-1">
+            </div>
+            <div class="col-sm-10">
+                <h3>Items</h3>
+                <div class="row">';
+$cpt = 0;
+foreach($items as $item)
+{
+    if($cpt == 3) {
+        echo '</div></br><div class="row">';
+        $cpt = 0;
     }
+    echo '<div class="col-sm-4">
+                <img id="' . $item->getName() . '" class="imgItem "
+                    src="http://ddragon.leagueoflegends.com/cdn/7.20.2/img/item/' . $item->getImage() . '" title="' . $item->getName() . '"
+                    alt="' . $item->getName() . '">
+          </div>';
+    $cpt++;
+}
 
-displayFin();
+echo '          </div>
+            </div>
+        </div>';
+
+echo '</div>';
+/*______ EFFET OMBRE  ____*/
+
+echo '<div class="madiv col-md-8">
+        <div class="topDiv"></div>
+        <div class="leftDiv"></div>
+        <div class="rightDiv"></div>
+        <div class="bottomDiv"></div>
+      </div>
+    </div>';
+
+displayFinAlea();
